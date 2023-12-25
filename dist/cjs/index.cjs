0e7d43aaabca7afea2c3aefb6f4568c5ea5f4e70
Nick <nickfriedrich@outlook.com>
2023-12-25 10:57:51 +0100

    ..

--- dist/cjs/index.cjs
+++ dist/cjs/index.cjs
@@ -1,9 +1,8 @@
-let scrape;
+let scrapePromise = import('../../src/aliexpressProductScraper.js')
+    .then((module) => module.scrape)
+    .catch(err => {
+        console.error("Aliexpress-product-scraper-ts Error: ", err);
+        throw err;
+    });
 
-import('../../src/aliexpressProductScraper.js').then((module) => {
-    scrape = module.scrape;
-    // Rest of your code that uses scrape
-}).catch(err => {
-    console.error("Aliexpress-product-scraper-ts Error: ", err);
-    // Handle the error
-});
+module.exports = { scrapePromise };
